0d3b3d68339b7773eae63361f9ba72a1a411e903
Michael Lee <mikeslee97@gmail.com>
2020-04-21 17:40:08 -0400

    Changed dialogue a bit, SpawnNPC script

---
+++ Assets/Scripts/SpawnNPC.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Spawns an NPC to the location of this object when player enters trigger
+public class SpawnNPC : MonoBehaviour
+{
+    public Transform NPCObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            NPCObject.position = transform.position;
+        }
+    }
+}
