98c72bb0b575b829c3ce38f223e9f1be052a134a
Michael Lee <mikeslee97@gmail.com>
2020-04-23 23:58:54 -0400

    small change to UI

--- Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -85,6 +85,7 @@ public class DialogueManager : MonoBehaviour
                 case "IMG":     // Set the image of character speaking
                     // Real path: Assets/Resources/...
                     ImageBox.sprite = Resources.Load<Sprite>(DequeueNextTag());
+                    Debug.Log(ImageBox.sprite != null);
                     ImageBox.gameObject.SetActive(ImageBox.sprite != null);
                     break;
                 case "QUEST":   // this must be the first tag of the "quest" tags
